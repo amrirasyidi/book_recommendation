6d2c8cf197927670000aa7d21c7a4055f40cf176
amri.rasyidi <amri.rasyidi0@gmail.com>
2022-09-22 10:10:48 +0700

    edit pickles

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
 
-search_result = pickle.load(open('pickles/search_result.pkl', 'rb'))
-search_define = pickle.load(open('pickles/search_define.pkl', 'rb'))
-search_fit_transform = pickle.load(open('pickles/search_fit_transform.pkl', 'rb'))
+
+
 
 # the starting point from where the app will run
 app=Flask(__name__)
 
 # Load the data set
 
+
 # Load the search algorithm
+search_define = pickle.load(open('pickles/search_define.pkl', 'rb'))
+search_fit_transform = pickle.load(open('pickles/search_fit_transform.pkl', 'rb'))
+search_result = pickle.load(open('pickles/search_result.pkl', 'rb'))
 
 # Load the collaborative algorithm
 
@@ -26,8 +29,8 @@ def home():
 
 @app.route('/search_api', methods=['POST'])
 def search_api():
-    user_input = request.form.get('user_input')
-    book_rec = search_result("harry potter",search_define)
+    user_input = request.form.get("user_input")
+    book_rec = search_result(user_input,search_define)
 
     print(book_rec)
 
