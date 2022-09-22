645b8bd390e3eb42b11a5b62dcd8f9666e593050
amri.rasyidi <amri.rasyidi0@gmail.com>
2022-09-22 17:44:13 +0700

    add test.html and images_data.pkl made the MVP for book search page

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import dill as pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, json, request, app, jsonify, url_for, render_template
+
+import re
+
 import numpy as np
 import pandas as pd
+
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 
 
 # the starting point from where the app will run
-app=Flask(__name__)
+app=Flask(__name__, template_folder='pages')
 
 # Load the data set
-
+books_data = pickle.load(open('pickles/books_data.pkl', 'rb'))
+images_data = pickle.load(open('pickles/images_data.pkl', 'rb'))
 
 # Load the search algorithm
 search_define = pickle.load(open('pickles/search_define.pkl', 'rb'))
@@ -20,22 +26,35 @@ search_result = pickle.load(open('pickles/search_result.pkl', 'rb'))
 # Load the collaborative algorithm
 
 
-# define the url address. '/' indicates homepage
-# landing page
+# Define the url address. '/' indicates homepage
+# Landing page
 @app.route('/')
 def home():
     return render_template('home.html')
-    # return "Hello World"
 
+# For postman API
 @app.route('/search_api', methods=['POST'])
 def search_api():
-    user_input = request.form.get("user_input")
-    book_rec = search_result(user_input,search_define)
+    data=request.json["data"]
+    print(data)
 
+    book_rec = search_result(data,search_define)
     print(book_rec)
 
+    return book_rec
+
+# For web app
+@app.route('/search', methods=['POST'])
+def search():
+    user_input = [str(i) for i in request.form.values()]
+    book_rec = search_result(user_input[0],search_define)
+
     return render_template('home.html',data=book_rec)
-    
+
+@app.route('/rec')
+def rec():
+    user_input = [str(i) for i in request.form.values()]
+    return render_template('test.html', forward_message = user_input)
 
 if __name__ == '__main__':
     app.run(debug = True)
